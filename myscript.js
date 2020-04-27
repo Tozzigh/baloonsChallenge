3da09ec2cb8991192bfc5f2a749b86556757be02
Tozzigh <tozzigh@gmail.com>
2020-04-27 22:43:10 +0000

    Ci siamo

--- myscript.js
+++ myscript.js
@@ -1,16 +1,30 @@
 var array = ["","green", "gray","red","white","blue","yellow"];
+var count = 1;
+var vite = 3;
+
 setInterval(function spawnBall() {
 	    this.div = document.createElement("div") ;
         document.body.appendChild( this.div ) ;
         this.div.className = "ball rounded-circle" ;
-        this.div.style.top = "90%";
-        this.div.style.left = parseInt( 100 * Math.random() ) + "%" ;
-        this.div.style.backgroundColor = array[Math.floor((Math.random() * 6) + 1)];
-        this.div.onclick= function () {
+        this.div.style.top = "110%";
+        this.div.style.left = parseInt( 90 * Math.random() ) + "%" ;
+        this.div.style.backgroundColor = array[Math.floor((Math.random() * 6) + 1)];    
+
+        this.div.onclick= function removeBall() {
             this.parentElement.removeChild(this);
+            document.getElementById("score").innerHTML=count;
+            count++;
         };
-        if(this.div.position().top == 100){
-            document.getElementById("life1").classList.add("d-none");
+        
+        if(vite == 3){
+            this.div.addEventListener("webkitAnimationEnd", function(){document.getElementById("life1").classList.add("d-none");})
+            vite--;
+        }else if(vite == 2){
+            this.div.addEventListener("webkitAnimationEnd", function(){document.getElementById("life2").classList.add("d-none");})
+            vite--;
+        }else if(vite == 1){
+            this.div.addEventListener("webkitAnimationEnd", function(){document.getElementById("life3").classList.add("d-none");})
+            vite--;
         }
         
     }, 900);
