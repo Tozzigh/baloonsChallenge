be5f36418e2fc6bef86b5378c5c9649df64f3e1c
Tozzigh <tozzigh@gmail.com>
2020-04-27 18:09:54 +0000

    Quasi

--- myscript.js
+++ myscript.js
@@ -1,8 +1,16 @@
+var array = ["","green", "gray","red","white","blue","yellow"];
 setInterval(function spawnBall() {
 	    this.div = document.createElement("div") ;
         document.body.appendChild( this.div ) ;
         this.div.className = "ball rounded-circle" ;
-        this.div.style.top = parseInt( 100 * Math.random() ) + "%" ;
+        this.div.style.top = "90%";
         this.div.style.left = parseInt( 100 * Math.random() ) + "%" ;
-        this.div.style.backgroundColor = "hsla("+parseInt( 360 * Math.random() )+", "+parseInt( 100 * Math.random() )+"%, "+parseInt( 100 * Math.random() )+"%, "+Math.random()+")";
-    }, 50);
+        this.div.style.backgroundColor = array[Math.floor((Math.random() * 6) + 1)];
+        this.div.onclick= function () {
+            this.parentElement.removeChild(this);
+        };
+        if(this.div.position().top == 100){
+            document.getElementById("life1").classList.add("d-none");
+        }
+        
+    }, 900);
