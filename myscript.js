5b7142b25b203a97a23b1347c0bffa3a6d6e7366
Tozzigh <tozzigh@gmail.com>
2020-04-27 14:57:39 +0000

    Screensaver

--- myscript.js
+++ myscript.js
@@ -1,35 +1,8 @@
-var count=19;
-var baloons=" baloons!!!";
-
-function countFun(){    
-    document.getElementById("showCount").innerHTML="Pop all the "+count+baloons;
-    if(count == 0){
-        document.getElementById("showCount").innerHTML= "YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!";
-        location.reload();
-    }
-    if(count == 1){
-        document.getElementById("showCount").innerHTML= "Pop the last baloon!!!";
-    }   
-    count-=1;
-}
-
-document.getElementById("1").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("2").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("3").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("4").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("5").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("6").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("7").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("8").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("9").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("10").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("11").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("12").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("13").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("14").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("15").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("16").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("17").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("18").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("19").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
-document.getElementById("20").src = 'icons8-party-balloon-10'+(Math.floor((Math.random() * 4) + 1)).toString()+'.png';
+setInterval(function spawnBall() {
+	    this.div = document.createElement("div") ;
+        document.body.appendChild( this.div ) ;
+        this.div.className = "ball rounded-circle" ;
+        this.div.style.top = parseInt( 100 * Math.random() ) + "%" ;
+        this.div.style.left = parseInt( 100 * Math.random() ) + "%" ;
+        this.div.style.backgroundColor = "hsla("+parseInt( 360 * Math.random() )+", "+parseInt( 100 * Math.random() )+"%, "+parseInt( 100 * Math.random() )+"%, "+Math.random()+")";
+    }, 50);
