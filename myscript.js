1f63d8bdc6a76f6ff944f12fcbc61a82afcac04e
Tozzigh <tozzigh@gmail.com>
2020-05-12 13:03:35 +0000

    Optimized

--- myscript.js
+++ myscript.js
@@ -2,29 +2,38 @@ var array = ["","green", "gray","red","white","blue","yellow"];
 var count = 1;
 var vite = 3;
 
-setInterval(function checkLife(){
-        if(vite == 3){
-            this.div.addEventListener("animationend", function(){document.getElementById("life1").className = "d-none";vite=2; })
-        }
-        if(vite == 2){
-            this.div.addEventListener("animationend", function(){document.getElementById("life2").className = "d-none";vite=1; })
-        }
-        if(vite == 1){
-            this.div.addEventListener("animationend", function(){document.getElementById("life3").className = "d-none";location.reload(); })
-        };
-}, 900);
+window.onload = () => {
 
-setInterval(function spawnBall() {
-	    this.div = document.createElement("div") ;
-        document.body.appendChild( this.div ) ;
-        this.div.className = "ball rounded-circle" ;
-        this.div.style.top = "100%";
-        this.div.style.left = ( 90 * Math.random() ) + "%" ;
-        this.div.style.backgroundColor = array[Math.floor((Math.random() * 6) + 1)];    
+    const threeLifes = (new Array(vite)).fill(null).map(x => "<li><img src='icons8-heart-64.png'></li>").join("");
+    document.querySelector(".lifeBar").innerHTML = threeLifes; 
 
-        this.div.onclick= function removeBall() {
-            this.parentElement.removeChild(this);
-            document.getElementById("score").innerHTML=count;
-            count++;
-        };
-}, 900);
+    const spawn = setInterval(function spawnBall() {
+        if ( document.hasFocus() ) {
+            const div = document.createElement("div") ;
+            document.body.appendChild( div ) ;
+            div.className = "ball rounded-circle" ;
+            div.style.top = "100%";
+            div.style.left = ( 90 * Math.random() ) + "%" ;
+            div.style.backgroundColor = array[Math.floor((Math.random() * 6) + 1)];    
+
+            div.addEventListener("click", function(ball){
+                ball.target.parentElement.removeChild(ball.target);
+                document.getElementById("score").innerHTML=count;
+                count++;
+            });
+
+            div.addEventListener("animationend", function(ball){
+                ball.target.parentElement.removeChild(ball.target);
+                vite--; 
+                document.querySelector("li").remove(); 
+                console.log(vite)           
+                if(vite<1){
+                    clearInterval(spawn);
+                    document.body.innerHTML = "<div class='gg'><h1>Game Over</h1><h2>"+count+"</h2><button onClick='window.location.reload()'>Retry</button></div>";
+                }
+            });
+        }
+    }, 900);
+    
+           
+}
